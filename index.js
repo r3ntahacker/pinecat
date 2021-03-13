ee63459367556521c106743cc26ecc9b0e538f26
Mimmo B <39281985+r3ntahacker@users.noreply.github.com>
2021-03-13 13:46:37 -0500

    doesnt work

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 	if (message.content === '!ping') {
 		message.channel.send('Pong.');
 	} else if (message.content === `${prefix}user-info`) {
     message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+  } else if (message.content === '!avatar') {
+    if (!message.mentions.users.size) {
+      return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+    }
+  } else if (message.content === 'im') { 
+    message.channel.send(`Hi ${args} im pinecat`);
   }
 });
-
