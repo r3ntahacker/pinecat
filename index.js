4a5fd25db1e932b0f79b3056895415b402fcfbe4
n3k0n3k0 <68358629+n3k0n3k0@users.noreply.github.com>
2021-03-13 14:19:06 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,29 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const ping = require('minecraft-server-util')
 const prefix = '!'
-client.login('token');
+require('dotenv').config()
+const token = process.env.pinecat_token;
 
 client.on('ready', () => {
   console.log(`${client.user.tag} is now alive pog!`);
 });
 
-client.on('message', message => {
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift[0]().toLowerCase();
-	if (message.content === '!ping') {
-		message.channel.send('Pong.');
-	} else if (message.content === `${prefix}user-info`) {
-    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-  } else if (message.content === '!avatar') {
-    if (!message.mentions.users.size) {
-      return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
-    }
-  } else if (message.content === 'im') { 
-    message.channel.send('Hi ${args} im pinecat');
-  }
-});
-client.on('message', msg => {
-  if (msg.content === 'im hungry') {
-    msg.channel.send('hi hungry im pinecat');
-  }
-});
